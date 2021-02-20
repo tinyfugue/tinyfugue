265bdab0ede9df849b8c58aaad7467b8c4a50ce5
Justin Coffman <jcoffman@sysguard.io>
2021-02-20 16:44:15 -0500

    Fix duplicate world_decl definition

--- src/socket.h
+++ src/socket.h
@@ -21,7 +21,7 @@
 #define CONN_BG		0x08
 #define CONN_FG		0x10
 
-struct World   *world_decl;	/* declares struct World */
+struct World;	/* declares struct World */
 
 extern String *incoming_text;
 extern int quit_flag;
